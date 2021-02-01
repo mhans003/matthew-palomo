158d166c48989766ac36057f6105fc0c44c6affa
mhans003 <michaeledwardhanson@gmail.com>
2021-02-01 13:47:54 -0800

    Continued styling with homepage/about section. Placeholder for image in place and page responsive to screen sizes; Footer remains at bottom and allows scrolling.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,11 +8,35 @@ import Col from "../components/Grid/Col";
 //Components
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import ContentBox from "../components/ContentBox";
 
 const Home = (props) => {
     return (
         <div>
             <Header current={props.current}/>
+            <ContentBox id={"about-box"}>
+                <Container>
+                    <h1 className="section-heading">A<span className="smaller-text">BOUT</span></h1>
+                    <hr className="orange-hr"/>
+                    <Row>
+                        <Col size="col-lg-6">
+                            IMAGE PLACHOLDER
+                        </Col>
+                        <Col size="col-lg-6">
+                            <p className="biobody mt-3 mt-lg-0">
+                                My name is Matthew Palomo, and I can help your business
+                                during this pandemic. 
+                            </p>
+                            <p className="biobody mt-3 mt-lg-0">
+                                I am offering <span className="orange">free</span> consultation in   
+                                these difficult times for companies looking to open/stay
+                                open while keeping employees and customers safe, following 
+                                Federal/State guidelines.
+                            </p>
+                        </Col>
+                    </Row>
+                </Container>
+            </ContentBox>
             <Footer/>
         </div>
     );
