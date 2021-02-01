3be55f9de4b8b457ea670fdf61ec4cdbb8beb8b5
mhans003 <michaeledwardhanson@gmail.com>
2021-02-01 12:36:00 -0800

    Styled navbar, navbar brand, links with new color scheme and background image with dark overlay.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,18 +5,27 @@ import NavItem from "../NavItem";
 
 function Header(props) {
     return (
-        <Navbar className="navbar-dark bg-dark py-0" expand="lg">
-            <Navbar.Brand className="py-5" href="/">Matthew Palomo</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" className="mr-2" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                    <NavItem text={"Home"} tagName={'home'} current={props.current}/>
-                    <NavItem text={"Services"} tagName={'services'} current={props.current}/>
-                    <NavItem text={"Credentials"} tagName={'credentials'} current={props.current}/>
-                    <NavItem text={"Contact"} tagName={'contact'} current={props.current}/>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        <div className="overlay-image">
+            <Navbar className="navbar-dark py-0" expand="lg">
+                    <Navbar.Brand className="py-5" href="/">
+                        <span className="brand-name">M. PALOMO</span>
+                        <br/>
+                        <span className="brand-sub">
+                            S<span className="smaller-text">AFETY </span> 
+                            C<span className="smaller-text">ONSULTATIONS</span>
+                        </span>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" className="mr-2" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
+                            <NavItem text={"ABOUT"} tagName={'home'} current={props.current}/>
+                            <NavItem text={"COVID-19"} tagName={'services'} current={props.current}/>
+                            <NavItem text={"CREDENTIALS"} tagName={'credentials'} current={props.current}/>
+                            <NavItem text={"CONTACT"} tagName={'contact'} current={props.current}/>
+                        </Nav>
+                    </Navbar.Collapse>
+            </Navbar>
+        </div>
     );
 }
 
